d3283da9220656504cbe2b7d5518c0982da920c6
SolarizedOrange <147306607+SolarizedOrange@users.noreply.github.com>
2025-11-16 19:50:34 +0900

    [Feat] Weapons

--- Assets/Scripts/Weapons/RangedWeapon.cs
+++ Assets/Scripts/Weapons/RangedWeapon.cs
@@ -4,15 +4,20 @@ public abstract class RangedWeapon: WeaponBase
 {
     public Transform MuzzlePosition;
 
-    public override void Attack()
+    public void Fire()
     {
-
+        Debug.Log("FIRE");
         if (Physics.Raycast(MuzzlePosition.position, MuzzlePosition.forward, out var hitInfo))
         {
+            Debug.Log("RayCastHit");
             var res = hitInfo.collider;
-            if (res.CompareTag("Enemy"))
+            if (res.TryGetComponent<HitBox>(out var hitBox))
             {
-                // res.GetComponent<EnemyController>().OnHit();
+                hitBox.OnHit();
+            }
+            else
+            {
+                
             }
         }
     }
