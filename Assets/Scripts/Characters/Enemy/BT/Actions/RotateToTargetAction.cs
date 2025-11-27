eae82827d786a405b231afe7d61cc83286da7bef
tjdeo1102 <57614322+tjdeo1102@users.noreply.github.com>
2025-11-27 20:16:27 +0900

    [Fix] Enemy Targeting Rotation

---
+++ Assets/Scripts/Characters/Enemy/BT/Actions/RotateToTargetAction.cs
@@ -0,0 +1,21 @@
+using System;
+using Unity.Behavior;
+using UnityEngine;
+using Action = Unity.Behavior.Action;
+using Unity.Properties;
+
+[Serializable, GeneratePropertyBag]
+[NodeDescription(name: "RotateToTarget", story: "[Agent] rotate to [Target]", category: "Action", id: "df05907a588d4c845f3669f3597c1942")]
+public partial class RotateToTargetAction : Action
+{
+    [SerializeReference] public BlackboardVariable<EnemyController> Agent;
+    [SerializeReference] public BlackboardVariable<CharacterBase> Target;
+
+    protected override Status OnStart()
+    {
+        var dir = Target.Value.transform.position - Agent.Value.transform.position;
+        Agent.Value.MoveCtrl.SetTargetRotation(dir.x * Vector3.right);
+        return Status.Success;
+    }
+}
+
