bf74e95702c556549445988f63d33696cc02297a
tjdeo1102 <57614322+tjdeo1102@users.noreply.github.com>
2025-12-23 17:13:17 +0900

    [Fix] Enemy Prefab & Scene
    - FIx Prefab , Scene
    - FIx story ReloadCondition

--- Assets/Scripts/Characters/Enemy/BT/Conditions/ReloadCondition.cs
+++ Assets/Scripts/Characters/Enemy/BT/Conditions/ReloadCondition.cs
@@ -3,7 +3,7 @@ using Unity.Behavior;
 using UnityEngine;
 
 [Serializable, Unity.Properties.GeneratePropertyBag]
-[Condition(name: "Reload", story: "[Agent]: Ammo < [Threshold] Flip: [True] RandomThreshold: [RandomThreshold]", category: "Conditions", id: "dd7e31801888496545960557e7a1e9ce")]
+[Condition(name: "Reload", story: "[Agent] : Ammo < [Threshold] Flip: [True] RandomThreshold: [RandomThreshold]", category: "Conditions", id: "dd7e31801888496545960557e7a1e9ce")]
 public partial class ReloadCondition : Condition
 {
     [SerializeReference] public BlackboardVariable<EnemyController> Agent;
